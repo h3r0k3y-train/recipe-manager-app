45e0cf5bce0eb98c5c485a7371de794e6db9ce3b
h3r0k3y-train <96561667+h3r0k3y-train@users.noreply.github.com>
2022-01-13 19:01:48 +0100

    cleanup

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,16 +1,14 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {exhaustMap, map, take, tap} from "rxjs/operators";
+import {HttpClient} from "@angular/common/http";
+import {map, tap} from "rxjs/operators";
 
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
-import {AuthService} from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
   constructor(private http: HttpClient,
-              private recipeService: RecipeService,
-              private authService: AuthService) {}
+              private recipeService: RecipeService) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
